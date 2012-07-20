20dc38c5f9aa20baaef65938f6e06a4b1f7e0e54
kilouco <alexandre.a.severino@gmail.com>
2012-07-20 00:23:29 -0300

    * Updated website info; * Improvements with the Creature List panel.

--- afk/creatureList.lua
+++ afk/creatureList.lua
@@ -37,4 +37,38 @@ function CreatureList.hide()
   parent.getUi():setEnabled(true)
 end
 
+function CreatureList.addBlack()
+  local text = creatureListWindow:getChildById('TextField'):getText()
+  local list = creatureListWindow:getChildById('BlackList')
+
+  local item = g_ui.createWidget('ListRow', list)
+  item:setText(text)
+end
+
+function CreatureList.addWhite()
+  local text = creatureListWindow:getChildById('TextField'):getText()
+  local list = creatureListWindow:getChildById('WhiteList')
+
+  local item = g_ui.createWidget('ListRow', list)
+  item:setText(text)
+end
+
+function CreatureList.remBlack()
+  local selected = creatureListWindow:getChildById('BlackList'):getFocusedChild()
+
+  if selected then
+    selected:destroy()
+    selected = nil
+  end
+end
+
+function CreatureList.remWhite()
+  local selected = creatureListWindow:getChildById('WhiteList'):getFocusedChild()
+
+  if selected then
+    selected:destroy()
+    selected = nil
+  end
+end
+
 return CreatureList
